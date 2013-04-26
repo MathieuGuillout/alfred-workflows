4e57d68df3c0d73f8fa26bf0bf42f2ad093bae38
MathieuGuillout <mathieu@creativelicence.com.au>
2013-04-26 17:25:04 +1000

    Initial commit

--- Kat/workflow/bundle/ruby/1.8/gems/hpricot-0.8.6/ext/fast_xs/siteconf20130421-24755-61efa-0.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "/Users/mattam/Documents/protos/AlfredHN/workflow/bundle/ruby/1.8/gems/hpricot-0.8.6/lib"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
